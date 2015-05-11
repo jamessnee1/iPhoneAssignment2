d5e835b56d306812af3a2c16c9b0c4ea706d4662
James Snee <jamessnee1@gmail.com>
2015-05-11 16:45:32 +1000

    first commit

---
+++ Quiz Fun/AppModel.swift
@@ -0,0 +1,13 @@
+//
+//  AppModel.swift
+//  Quiz Fun
+//
+//  Created by James Snee on 11/05/2015.
+//  Copyright (c) 2015 James Snee and Heather Ingram. All rights reserved.
+//
+
+import UIKit
+
+class AppModel: NSObject {
+   
+}
---
+++ Quiz Fun/Category.swift
@@ -0,0 +1,9 @@
+//
+//  Category.swift
+//  Quiz Fun
+//
+//  Created by James Snee on 11/05/2015.
+//  Copyright (c) 2015 James Snee and Heather Ingram. All rights reserved.
+//
+
+import Foundation
