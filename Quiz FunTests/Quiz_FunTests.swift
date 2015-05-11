8b9c0d0f552d97533558263303d8de7b18180397
James Snee <s3369721@b01409015-21.cs.rmit.edu.au>
2015-05-11 14:39:02 +1000

    Initial Commit

---
+++ Quiz FunTests/Quiz_FunTests.swift
@@ -0,0 +1,36 @@
+//
+//  Quiz_FunTests.swift
+//  Quiz FunTests
+//
+//  Created by James Snee on 11/05/2015.
+//  Copyright (c) 2015 James Snee and Heather Ingram. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Quiz_FunTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
